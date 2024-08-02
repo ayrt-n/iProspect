e949b1240002cac851a41ba5270ed7b57bee54f3
ayrton <ayrton.parkinson1@gmail.com>
2024-08-02 10:29:56 -0400

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Ayrton Parkinson on 2024/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Ayrton Parkinson on 2024/08/02.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
