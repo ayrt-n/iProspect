0cba9b9b37911ee13027e9cb05f0ac92aed82816
ayrton <ayrton.parkinson1@gmail.com>
2024-08-03 09:01:28 -0400

    Initial commit

---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,22 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Ayrton Parkinson on 2024/08/02.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Prospect {
+    var name: String
+    var emailAddress: String
+    var isContacted: Bool
+    
+    init(name: String, emailAddress: String, isContacted: Bool) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.isContacted = isContacted
+    }
+}
